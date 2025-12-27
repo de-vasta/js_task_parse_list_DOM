aff5a092bf63885de7834f7b3b8c87a1efe2a904
Nazar Kundys <nazarrisd@gmail.com>
2025-12-27 17:28:49 +0200

    make build

---
+++ js_task_parse_list_DOM.f75de5e1.js
@@ -0,0 +1,35 @@
+'use strict';
+const employeeList = document.querySelector('ul');
+const employees = Array.from(employeeList.children);
+const employeesBySalary = sortList(employees, 'salary');
+const employeeData = getEmployees(employeesBySalary);
+const cardsContainer = document.createElement('div');
+employeeData.forEach((emp)=>{
+    const card = document.createElement('div');
+    card.innerHTML = `
+    <h3>${emp.name}</h3>
+    <p>Position: ${emp.position}</p>
+    <p>Salary: $${emp.salary.toLocaleString()}</p>
+    <p>Age: ${emp.age}</p>
+  `;
+    cardsContainer.appendChild(card);
+});
+document.body.appendChild(cardsContainer);
+function sortList(list, criteria) {
+    const sorted = list.sort((a, b)=>parseNumber(b.dataset[criteria]) - parseNumber(a.dataset[criteria]));
+    employeeList.replaceChildren(...sorted);
+    return sorted;
+}
+function getEmployees(list) {
+    return list.map((li)=>({
+            name: li.textContent.trim(),
+            position: li.dataset['position'],
+            salary: parseNumber(li.dataset['salary']),
+            age: parseNumber(li.dataset['age'])
+        }));
+}
+function parseNumber(numStr) {
+    return Number(numStr.replace(/\D/g, ''));
+}
+
+//# sourceMappingURL=js_task_parse_list_DOM.f75de5e1.js.map
